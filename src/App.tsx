75c1d4d4246850e91c2fd0efffc629c5d110fc3d
Karimdebza <debzakarim10@gmail.com>
2026-03-27 16:10:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Footer from "./components/Footer";
+// import ProjectCard from "./components/ProjectCard";
+
+export default function App() {
+  return (
+    <div className="font-sans text-gray-900 bg-white">
+      <Navbar />
+      <Hero />
+      {/* <About /> */}
+      
+      {/* <Footer /> */}
+    </div>
+  );
+}
---
+++ src/data/Projects.ts
@@ -0,0 +1,17 @@
+export type Projects = {
+  title: string;
+  description: string;
+  tech: string[];
+  github: string;
+  demo: string;
+};
+
+export const projects: Projects[] = [
+  {
+    title: "Puzzle Game",
+    description: "Jeu interactif avec logique avancée.",
+    tech: ["React", "Node.js"],
+    github: "#",
+    demo: "#"
+  }
+];
